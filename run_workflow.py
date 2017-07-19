928b09fa8bea92b0e128e47017bb43dda4e60faa
charlie butcosk <cbutcosk@users.noreply.github.com>
2017-07-19 12:21:12 -0400

    Commit w/ travis workflow
    Basic CI using ISI karma workflows

---
+++ run_workflow.py
@@ -0,0 +1,24 @@
+
+
+from workflow_config import REPO_CONFIG, base_uri
+from subprocess import call
+from os.path import join
+
+for repo in REPO_CONFIG:
+    name = repo['name']
+    input_file_type = repo['input_file_type']
+    input_file = repo['input_file']
+    model_file = repo['model_file']
+
+
+    call(['java','-cp','karma-spark-0.0.1-SNAPSHOT-shaded.jar','edu.isi.karma.rdf.OfflineRdfGenerator',
+        '--sourcetype',input_file_type,
+        '--filepath',join(name,input_file),
+        '--modelfilepath',join(name,model_file),
+        '--sourcename','ccma',
+        '--outputfile','test.rdf']
+        )
+    # FIXME: make this n3 generation 
+
+# Concetenate all data
+# Zip it
---
+++ workflow_config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import logging
+
+LOG_LEVEL = logging.INFO
+ZIP_OUTPUT = True
+
+repo_name = 'ccma'
+repo_path = './../../aac-repos/' + repo_name
+base_uri = 'http://data.americanartcollaborative.org/ccma/'
+context_uri = 'https://github.com/american-art/aac-alignment/blob/master/karma-context.json'
+REPO_CONFIG = [
+    {
+        'path': repo_path,
+        'name': 'ccma_artists',
+        'base_uri': base_uri,
+        'rdf_root_uri': 'http://www.cidoc-crm.org/cidoc-crm/E39_Actor1',
+        'context_uri': context_uri,
+        'model_file': 'ccma_artists-model.ttl',
+        'input_file': 'ccma_artists.json',
+        'input_file_type': 'json',
+        'output_file_name': 'ccma_artists'
+    },
+    {
+        'path': repo_path,
+        'name': 'ccma_objects',
+        'base_uri': base_uri,
+        'rdf_root_uri': 'http://www.cidoc-crm.org/cidoc-crm/E22_Man-Made_Object1',
+        'context_uri': context_uri,
+        'model_file': 'ccma_objects-model.ttl',
+        'input_file': 'ccma_objects.json',
+        'input_file_type': 'json',
+        'output_file_name': 'ccma_objects'
+    }
+]
