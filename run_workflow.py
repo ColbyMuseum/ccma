8ca6b3134fdbade90fe0c52c633efba619bc010d
charlie butcosk <cbutcosk@users.noreply.github.com>
2017-07-19 13:22:04 -0400

    Fix output for releases

--- run_workflow.py
+++ run_workflow.py
@@ -8,14 +8,13 @@ for repo in REPO_CONFIG:
     input_file = repo['input_file']
     model_file = repo['model_file']
 
-
     call(['java','-cp','karma-spark-0.0.1-SNAPSHOT-shaded.jar','edu.isi.karma.rdf.OfflineRdfGenerator',
         '--baseuri',base_uri,
         '--sourcetype',input_file_type,
         '--filepath',join(name,input_file),
         '--modelfilepath',join(name,model_file),
         '--sourcename','ccma',
-        '--outputfile','test.rdf']
+        '--outputfile','output/test.rdf']
         )
     # FIXME: make this n3 generation 
 
